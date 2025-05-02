f6378ece859d93fbae90107c00951bb792e4228b
0y2k <0x0y2k@gmail.com>
2025-05-03 08:38:02 +0900

    fix(astro.config.mjs): Provide correct GitHub URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
         {
           icon: "github",
           label: "GitHub",
-          href: "https://github.com/withastro/starlight",
+          href: "https://github.com/0y2k/outstanding-engineer",
         },
       ],
       sidebar: [
