3d29f6a8149be5d0c2c1cc01491572be9fccb1ab
0y2k <0x0y2k@gmail.com>
2025-05-03 08:09:42 +0900

    ci(GitHub Actions): Deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://0y2k.github.io",
+  base: "/outstanding-engineer",
   integrations: [
     starlight({
       title: "Outstanding Engineer",
